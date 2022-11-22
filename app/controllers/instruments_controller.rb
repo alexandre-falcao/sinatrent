0a3e2b23db5f11a15d62c3c5b0255294833ce695
alexandre falcao <alexmfal@gmail.com>
2022-11-22 11:51:22 -0300

    updating the create method for restaurant controller

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -9,11 +9,12 @@ class InstrumentsController < ApplicationController
   end
 
   def create
-    @user_id = current_user.id
-    @name = params[:instrument][:name]
-    @instrument_type = params[:instrument][:instrument_type]
-    @price = params[:instrument][:price]
-    @instrument = Instrument.new(name: @name, instrument_type: @instrument_type, price: @price, user_id: @user_id)
+    # @user_id = current_user.id
+    # @name = params[:instrument][:name]
+    # @instrument_type = params[:instrument][:instrument_type]
+    # @price = params[:instrument][:price]
+    @instrument = Instrument.new(instrument_params)
+    @instrument.user = current_user
     @instrument.save
     redirect_to root_path
   end
@@ -21,7 +22,7 @@ class InstrumentsController < ApplicationController
   private
 
   def instrument_params
-    params.require(:instrument).permit(:name, :type, :price, :status, :user_id)
+    params.require(:instrument).permit(:name, :type, :price)
   end
 
 end
