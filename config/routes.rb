5f16f91c278a9dde9d825e2eea31bcb64ecf8ea4
Matheus Araripe <matheus.alc2001@gmail.com>
2022-11-24 19:27:20 -0300

    fix end

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :instruments, only: [:new, :create, :index, :show] do
     resources :bookings, only: [:new, :create]
- 
+  end
   delete "instruments/:id", to: "instruments#destroy"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
