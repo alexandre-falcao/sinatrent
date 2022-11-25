6fa93723e9fe59a4ba99c3e680ab2f78f1b19d4b
Matheus Araripe <matheus.alc2001@gmail.com>
2022-11-24 22:00:33 -0300

    add the booking model

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,19 +10,36 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
+
+    #array_bookings = Booking.all
+
+    #bookings = false
+
+    # array_bookings.each do |booking|
+    #   range = (booking[:start_date]..booking[:return_date])
+
+    #   range_user = (@booking[:start_date]..@booking[:return_date])
+
+    #   if range_user.overlaps?range
+    #     bookings = true
+    #     redirect_to instruments_path
+    #   end
+    # end
+
+    #if bookings == false
     @booking.instrument = @instrument
     @booking.user = current_user
-    raise
+
     @booking.save
     authorize @instrument
     redirect_to instrument_path(@instrument)
+    #end
   end
 
   def instrument_available?
     # Achar o instrumentos
     @instrument = Instrument.find(params[:instrument_id])
     # Puxar a tabela de bookings
-
   end
 
   private
@@ -34,5 +51,4 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:booking).permit(:start_date, :return_date)
   end
-
 end
